e5b1b1f72043f44d78bf9cdf9e71dcab17812469
ygimenez <yagogimenez1@hotmail.com>
2021-06-14 11:42:30 -0300

    Added per-page custom buttons do paginate method

--- src/main/java/com/github/ygimenez/exception/NoIdentifierException.java
+++
@@ -1,9 +0,0 @@
-package com.github.ygimenez.exception;
-
-
-public class NoIdentifierException extends RuntimeException {
-
-    public NoIdentifierException() {
-        super("All custom buttons must have an ID.");
-    }
-}
