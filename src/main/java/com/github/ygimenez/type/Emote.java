6ef4409cd356a5d034fed78bf1de3db3202a8203
Hammy <sgoudham@gmail.com>
2021-07-17 04:30:52 +0100

    Add methods for updating/modifying reactions within Emote.java
    This implementation is honestly really ugly, not a fan of extracting it out into here

--- src/main/java/com/github/ygimenez/type/Emote.java
+++ src/main/java/com/github/ygimenez/type/Emote.java
@@ -1,39 +1,132 @@
 package com.github.ygimenez.type;
 
+import com.github.ygimenez.model.Page;
+import net.dv8tion.jda.api.entities.Message;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+import static com.github.ygimenez.method.Pages.clearReactions;
+import static com.github.ygimenez.method.Pages.updatePage;
+
+/**
+ * {@link EmoteWorker} specifies methods that should be overridden by the {@link Emote}'s
+ */
+interface EmoteWorker {
+    default int doWork(Message message, int pageIndex, int lastPageIndex, List<Page> pageList) { return 0; }
+    default int doWork(Message message, int pageIndex, int lastPageIndex, int skipAmount, List<Page> pageList) { return 0; }
+    default void doWork(Message message, Consumer<Void> successConsumer) {
+        clearReactions(message, successConsumer);
+    }
+}
+
 /**
  * Enumerator representing values required by non-dynamic buttons.
  */
-public enum Emote {
-	/**
-	 * {@link Emote} representing the "next" button (default: ▶).
-	 */
-	NEXT,
-	/**
-	 * {@link Emote} representing the "previous" button (default: ◀).
-	 */
-	PREVIOUS,
-	/**
-	 * {@link Emote} representing the "accept" button (default: ✅).
-	 */
-	ACCEPT,
-	/**
-	 * {@link Emote} representing the "cancel" button (default: ❎).
-	 */
-	CANCEL,
-	/**
-	 * {@link Emote} representing the "skip forward" button (default: ⏩).
-	 */
-	SKIP_FORWARD,
-	/**
-	 * {@link Emote} representing the "skip backward" button (default: ⏪).
-	 */
-	SKIP_BACKWARD,
-	/**
-	 * {@link Emote} representing the "go to first" button (default: ⏮).
-	 */
-	GOTO_FIRST,
-	/**
-	 * {@link Emote} representing the "go to last" button (default: ⏭).
-	 */
-	GOTO_LAST
+public enum Emote implements EmoteWorker {
+    /**
+     * {@link Emote} representing the "next" button (default: ▶).
+     */
+    NEXT {
+        @Override
+        public int doWork(Message message, int pageIndex, int lastPageIndex, List<Page> pageList) {
+            if (pageIndex < lastPageIndex) {
+                pageIndex++;
+                Page newPage = pageList.get(pageIndex);
+
+                updatePage(message, newPage);
+            }
+            return pageIndex;
+        }
+    },
+
+    /**
+     * {@link Emote} representing the "previous" button (default: ◀).
+     */
+    PREVIOUS {
+        @Override
+        public int doWork(Message message, int pageIndex, int lastPageIndex, List<Page> pageList) {
+            if (pageIndex > 0) {
+                pageIndex--;
+                Page newPage = pageList.get(pageIndex);
+
+                updatePage(message, newPage);
+            }
+            return pageIndex;
+        }
+    },
+
+    /**
+     * {@link Emote} representing the "accept" button (default: ✅).
+     */
+    ACCEPT { },
+
+    /**
+     * {@link Emote} representing the "cancel" button (default: ❎).
+     */
+    CANCEL { },
+
+    /**
+     * {@link Emote} representing the "skip forward" button (default: ⏩).
+     */
+    SKIP_FORWARD {
+        @Override
+        public int doWork(Message message, int pageIndex, int lastPageIndex, int skipAmount, List<Page> pageList) {
+            if (pageIndex < lastPageIndex) {
+                pageIndex += (pageIndex + skipAmount > lastPageIndex ? lastPageIndex - pageIndex : skipAmount);
+                Page page = pageList.get(pageIndex);
+
+                updatePage(message, page);
+            }
+            return pageIndex;
+        }
+    },
+
+    /**
+     * {@link Emote} representing the "skip backward" button (default: ⏪).
+     */
+    SKIP_BACKWARD {
+        @Override
+        public int doWork(Message message, int pageIndex, int lastPageIndex, int skipAmount, List<Page> pageList) {
+            if (pageIndex > 0) {
+                pageIndex -= (pageIndex - skipAmount < 0 ? pageIndex : skipAmount);
+                Page page = pageList.get(pageIndex);
+
+                updatePage(message, page);
+            }
+            return pageIndex;
+        }
+    },
+
+    /**
+     * {@link Emote} representing the "go to first" button (default: ⏮).
+     */
+    GOTO_FIRST {
+        @Override
+        public int doWork(Message message, int pageIndex, int lastPageIndex, List<Page> pageList) {
+            if (pageIndex > 0) {
+                pageIndex = 0;
+                Page page = pageList.get(pageIndex);
+
+                updatePage(message, page);
+            }
+            return pageIndex;
+        }
+    },
+
+    /**
+     * {@link Emote} representing the "go to last" button (default: ⏭).
+     */
+    GOTO_LAST {
+        @Override
+        public int doWork(Message message, int pageIndex, int lastPageIndex, List<Page> pageList) {
+            if (pageIndex < lastPageIndex) {
+                pageIndex = lastPageIndex;
+                Page page = pageList.get(pageIndex);
+
+                updatePage(message, page);
+            }
+            return pageIndex;
+        }
+    }
 }
